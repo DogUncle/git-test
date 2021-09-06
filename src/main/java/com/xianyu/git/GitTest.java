853fb15d66e051f13ec43980eb6252bc3ec7a29d
WangHong <tnuu@qq.com>
2021-09-06 13:09:48 +0800

    first commit

---
+++ src/main/java/com/xianyu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.xianyu.git;
+
+/**
+ * @program: git-test
+ * @description
+ * @author: wh
+ * @create: 2021-09-06 13:07
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
